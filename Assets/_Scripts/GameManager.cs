ed041416371f2d54fb323e2d4befb0e4499ece7c
Bảo Nguyễn Hoàng Gia <nguyenhoanggiabao17082006@gmail.com>
2026-09-24 16:44:10 +0700

    Update core logic

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -2,13 +2,35 @@
 
 public class GameManager : MonoBehaviour
 {
+    public static int Money;
+    public int startMoney = 100;
+
+    public static int Lives;
+    public int startLives = 20;
+
+    public static bool GameIsOver = false; 
+
     void Start()
     {
-        
+        GameIsOver = false; 
+        Money = startMoney;
+        Lives = startLives;
     }
 
     void Update()
     {
-        
+        if (GameIsOver)
+            return;
+
+        if (Lives <= 0)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        GameIsOver = true; 
+        Debug.Log("GAME OVER!"); 
     }
 }
